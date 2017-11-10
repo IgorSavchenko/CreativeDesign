10c1e59b8e3417ff4c082721e46ebdcf8de3bd4d
IgorSavchenko <ig0r@ua.fm>
2017-11-10 11:54:59 +0200

    Gamburger menu fixed. Services section fixed. Svg images added. Second slider added.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,24 +10,66 @@ HAMBURGER.click(() => {
 });
 //======================================================
 // menu navigation status change using jQuery
-$('.header__menu-link').on('click', function() {
-  $('.header__menu-link').removeClass('header__menu-link--active');
-  $(this).addClass('header__menu-link--active');
+$('.navbar-menu .navbar-item').on('click', function() {
+  $('.navbar-menu .navbar-item').removeClass(ACTIVE_CLASS);
+  $(this).addClass(ACTIVE_CLASS);
 });
 //======================================================
 // siema slider
 import Siema from 'siema';
 //default function
-function onInitCallback() {
-  console.log('Siema initialised bro :)');
-}
+// function onInitCallback() {
+//   console.log('Siema initialised bro :)');
+// }
 //default function
-function onChangeCallback() {
-  console.log(`The index of current slide is: ${this.currentSlide}`);
+// function onChangeCallback() {
+//   console.log(`The index of current slide is: ${this.currentSlide}`);
+// }
+
+Siema.prototype.addArrows = function() {
+
+  // make buttons & append them inside Siema's container
+  this.prevArrow1 = document.querySelector('.slider-button--prev');
+  this.nextArrow1 = document.querySelector('.slider-button--next');
+  this.prevArrow2 = document.querySelector('.slider-button--prev--dark');
+  this.nextArrow2 = document.querySelector('.slider-button--next--dark');
+  // this.prevArrow.textContent = 'previous slide';
+  // this.prevArrow.classList.add('slider-button', 'slider-button--prev');
+  // this.nextArrow.textContent = 'next slide';
+  // this.nextArrow.classList.add('slider-button', 'slider-button--next');
+  // this.selector.appendChild(this.prevArrow)
+  // this.selector.appendChild(this.nextArrow)
+
+  // event handlers on buttons
+  this.prevArrow1.addEventListener('click', () => this.prev());
+  this.nextArrow1.addEventListener('click', () => this.next());
+  this.prevArrow2.addEventListener('click', () => this.prev());
+  this.nextArrow2.addEventListener('click', () => this.next());
+}
+// Add a function that generates pagination to Siema
+function addNavigation() {
+  let length = this.innerElements.length;
+  for (let i = 0; i < length; i++) {
+    const BTN = document.createElement('button');
+    BTN.classList.add('slider-navigation-button');
+    if (i == 0) BTN.classList.add('slider-navigation-button--active');
+    BTN.addEventListener('click', () => this.goTo(i));
+    this.selector.nextSibling.appendChild(BTN);
+  }
+}
+// Add a function that change buttons in slider
+function setActiveButton(){
+  document.querySelectorAll('.slider-navigation-button').forEach((b, i) => {
+    if (i == (this.currentSlide || 0)){
+      b.classList.add("slider-navigation-button--active");
+    } else {
+      b.classList.remove("slider-navigation-button--active");
+    }
+  });
 }
 //init slider1
 const MY_SIEMA1 = new Siema({
-  selector: '.slider',
+  selector: '.works .slider',
   perPage: 1,
   startIndex: 0,
   loop: true,
@@ -36,52 +78,21 @@ const MY_SIEMA1 = new Siema({
   onInit: addNavigation,
   onChange: setActiveButton
 });
-Siema.prototype.addArrows = function() {
-
-  // make buttons & append them inside Siema's container
-  this.prevArrow = document.createElement('button');
-  this.nextArrow = document.createElement('button');
-  this.prevArrow.textContent = 'previous slide';
-  this.prevArrow.classList.add('button--prev');
-  this.nextArrow.textContent = 'next slide';
-  this.nextArrow.classList.add('button--next');
-  this.selector.appendChild(this.prevArrow)
-  this.selector.appendChild(this.nextArrow)
-
-  // event handlers on buttons
-  this.prevArrow.addEventListener('click', () => this.prev());
-  this.nextArrow.addEventListener('click', () => this.next());
-}
+// listen for keydown event
+setInterval(() => MY_SIEMA1.next(), 5000);
+//add arrows to slider1
 MY_SIEMA1.addArrows();
 //init slider2
-// const MY_SIEMA2 = new Siema({
-//   selector: '.team .slider',
-//   onInit: addNavigation,
-//   onChange: setActiveButton,
-// });
-// listen for keydown event
-// setInterval(() => MY_SIEMA1.next(), 4000);
-// Add a function that generates pagination to Siema
-function addNavigation() {
-  let length = this.innerElements.length;
-  for (let i = 0; i < length; i++) {
-    const BTN = document.createElement('button');
-    BTN.classList.add('slider-button');
-    if (i == 0) BTN.classList.add('slider-button--active');
-    BTN.addEventListener('click', () => this.goTo(i));
-    this.selector.nextSibling.appendChild(BTN);
-  }
-}
-// Add a function that change buttons in slider
-function setActiveButton(){
-  document.querySelectorAll('.slider-button').forEach((b, i) => {
-    if (i == (this.currentSlide || 0)){
-      b.classList.add("slider-button--active");
-    } else {
-      b.classList.remove("slider-button--active");
-    }
-  });
-}
+const MY_SIEMA2 = new Siema({
+  selector: '.team .slider',
+  perPage: 3,
+  startIndex: 0,
+  loop: true,
+  draggable: true,
+  duration: 500
+});
+//add arrows to slider2
+MY_SIEMA2.addArrows();
 //======================================================
 //smooth scroll effect
 // Select all links with hashes
@@ -143,18 +154,24 @@ initMap();
 //======================================================
 // Scroll Reveal effects
 window.sr = ScrollReveal({ reset: true , mobile: true});
-sr.reveal( '.navbar', { duration: 500 }, 250);
-// sr.reveal('.navbar-item', { container: '.navbar-menu' , duration: 2000});
+// sr.reveal( '.navbar', { duration: 500 }, 250);
+sr.reveal('.navbar-item', { container: '.navbar-brand' , duration: 2000});
 // sr.reveal('.navbar-item', { container: '.navbar' , rotate: {y: 90}, duration: 2500});
-sr.reveal('.about', { duration: 500 , viewOffset: { top: 100, right: 0, bottom: 100, left: 0 }});
-sr.reveal('.works', { duration: 500 , viewOffset: { top: 100, right: 0, bottom: 100, left: 0 }});
-sr.reveal('.contact', { duration: 500 , origin: 'left', distance: '500px'});
+// sr.reveal('.about', { duration: 500 , viewOffset: { top: 100, right: 0, bottom: 100, left: 0 }});
+// sr.reveal('.works', { duration: 500 , viewOffset: { top: 100, right: 0, bottom: 100, left: 0 }});
+// sr.reveal('.contact', { duration: 500 , origin: 'left', distance: '500px'});
 //======================================================
 // animation of social links icons on hover using .css animation
-$('.footer__social-link').mouseover(function() {
+$('.footer-social-link img').mouseover(function() {
   $(this).addClass('animated tada');
 });
-$('.footer__social-link').mouseleave(function() {
+$('.footer-social-link img').mouseleave(function() {
   $(this).removeClass('animated tada');
 });
 //======================================================
+$('.navbar-brand .navbar-item svg').mouseover(function() {
+  $(this).addClass('animated rubberBand');
+});
+$('.navbar-brand .navbar-item svg').mouseleave(function() {
+  $(this).removeClass('animated rubberBand');
+});
